2eba3fbceb47003c585f4cbd352da8598f70fb95
Fábio Sala <ofabiosala@gmail.com>
2024-08-10 17:43:16 -0300

    (feat): adição de entrada de dados e controle de fluxo ao menu.

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,5 +1,28 @@
 ﻿ShowProgramName();
+
 ShowProgramMenu();
+
+string optionSelected = Console.ReadLine()!;
+
+if (optionSelected == "1")
+{
+    Console.WriteLine("OPÇÃO " + optionSelected + " SELECIONADA.");
+}
+
+if (optionSelected == "2")
+{
+    Console.WriteLine("OPÇÃO " + optionSelected + " SELECIONADA.");
+}
+
+if (optionSelected == "3")
+{
+    Console.WriteLine("OPÇÃO " + optionSelected + " SELECIONADA.");
+}
+
+if (optionSelected == "0")
+{
+    Console.WriteLine("OPÇÃO " + optionSelected + " SELECIONADA.");
+}
 
 void ShowProgramName()
 {
@@ -23,4 +46,8 @@ void ShowProgramMenu()
     Console.WriteLine("[2] - EXIBIR BANDAS");
     Console.WriteLine("[3] - AVALIAR BANDA");
     Console.WriteLine("[0] - SAIR");
+
+    Console.WriteLine("");
+
+    Console.Write("DIGITE UMA OPÇÃO: ");
 }
--- ScreenSound/obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound/obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9dc4c66476243de8eb9c77ccc33e12fc78c005d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9703cd1407fc24454d86157d31d8f5cbe2a19dc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
